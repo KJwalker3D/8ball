f15ab0a3196449040f11211ae4cc67ac68bc8175
KatherineJoelle <walkerkatherinejoelle@gmail.com>
2025-04-07 00:11:33 +0200

    adjust welcome screen time

--- WelcomeGuiLocalScript.lua
+++ WelcomeGuiLocalScript.lua
@@ -1,6 +1,6 @@
 local gui = script.Parent
 local frame = gui:WaitForChild("Frame")
-wait(2) -- Delay to cover load
+wait(1) -- Delay to cover load
 frame:TweenSize(UDim2.new(0, 0, 1, 0), "Out", "Quad", 0.5, true)
 wait(0.5)
 gui.Enabled = false
