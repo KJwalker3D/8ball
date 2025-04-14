06e57abac1b1fbe6b486e2c4c9eeab49d0e0c225
KatherineJoelle <walkerkatherinejoelle@gmail.com>
2025-04-14 20:24:12 +0200

    custom animation ids

---
+++ CustomAnimationIDs.lua
@@ -0,0 +1,5 @@
+
+local sit01 = 117958790087422 -- wide legs and arms
+local sit02 = 73705578251202 -- laying on stomach with head on hands -- crappy
+local sit03 = 86638258300529 -- laying on stomach with head on hands working (not perfect but its ok)
+local sit04 = 103631876156765
